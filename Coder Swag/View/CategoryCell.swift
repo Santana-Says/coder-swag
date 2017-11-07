9d193717a43a1fe479c9ef6f36697b2ab53897f8
Jefffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-11-07 07:53:54 -0500

    Added asssests, began categories UI, created a cell view

---
+++ Coder Swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  Coder Swag
+//
+//  Created by Jefffrey Santana on 11/7/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImg: UIImageView!
+    @IBOutlet weak var categoryTitleLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
